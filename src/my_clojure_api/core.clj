c7837a7bb6ad784c2d232858ba28d297f2c8ae0d
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2024-11-26 07:59:27 -0300

    chore: configure dependencies

--- src/my_clojure_api/core.clj
+++ src/my_clojure_api/core.clj
@@ -1,7 +1,12 @@
 (ns my-clojure-api.core
-  (:gen-class))
+  (:gen-class :require [org.httpkit.server :refer [run-server]]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn app [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    (str "Salve He4rt Developers <3")})
