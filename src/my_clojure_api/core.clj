306e4c788ba301934ca01aaeb8560b47591b8c56
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2024-12-11 22:12:05 -0300

    add configs

--- src/my_clojure_api/core.clj
+++ src/my_clojure_api/core.clj
@@ -1,12 +1,12 @@
 (ns my-clojure-api.core
   (:gen-class :require [org.httpkit.server :refer [run-server]]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def supabase-config
+  {:url "https://xfmwwaqypjiqalpgqamr.supabase.co"
+   :api-key "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhmbXd3YXF5cGppcWFscGdxYW1yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM3OTk2NzksImV4cCI6MjA0OTM3NTY3OX0.Qx-Q7XT0X8F_BHWmL5p7Gb-BpNbvUxxDtjG8sgR0HPE"})
 
-(defn app [req]
-  {:status  200
-   :headers {"Content-Type" "text/html"}
-   :body    (str "Salve He4rt Developers <3")})
+(def news-config
+  {:url "https://api.nytimes.com/svc/news/v3/content/all/all.json"
+   :api-key "1TH9vj0F5Wy6fgJRKAWcVnGbKAIsSCLT"})
+
+
